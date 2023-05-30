05ae45eb834303aab9de8a065efac22c005c9898
estevamnetof <estevam.neto07@gmail.com>
2023-05-30 20:31:32 -0300

    adicionando arquivos da aula

---
+++ cursoPhp/tiposDeDados/array.php
@@ -0,0 +1,21 @@
+<?php
+    $varArrayNormal = [1,2,3,4,5];
+    echo 'Mostrando um array normal: <br>';
+    echo "Valor do primeiro item: $varArrayNormal[0] <br>";
+    echo "Valor do primeiro item: $varArrayNormal[1] <br>";
+    echo "Valor do primeiro item: $varArrayNormal[2] <br>";
+    echo "Valor do primeiro item: $varArrayNormal[3] <br>";
+    echo "Valor do primeiro item: $varArrayNormal[4] <br>";
+    echo "<br>";
+    echo "<br>";
+
+    $varArray = ['quantidadeFrutas' => 3 , 'quantidadeVerduras' => 2, 'quantidadeOutros' => 10];
+                 
+    echo "Array associativo: <br>";
+    echo "Valor do primeiro item (quantidadeFrutas): $varArray[quantidadeFrutas] <br>"; 
+    echo "Valor do segundo item (quantidadeVerduras): $varArray[quantidadeVerduras] <br>"; 
+    echo "Valor do terceiro item (quantidadeOutros): $varArray[quantidadeOutros] <br>"; 
+
+
+
+?>
