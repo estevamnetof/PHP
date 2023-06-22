76b87bdc38ffe524b206dbb6b5ff1be3c5be646e
estevamnetof <estevam.neto07@gmail.com>
2023-06-22 19:32:44 -0300

    aula de php de terça

--- cursoPhp/strings/testePercorrerString.php
+++ cursoPhp/strings/testePercorrerString.php
@@ -1,6 +1,8 @@
 <?php
 
-$str = "String";
+$str = "Curso Php";
+
+echo 'O comprimento da variável é ' . strlen($str) . '<br>';
 
 for ($i = 0; $i < strlen($str); $i++) {
     echo "Elemento $i: " . $str[$i] . "<br>"
